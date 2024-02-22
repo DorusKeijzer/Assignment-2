f4ed5ab333ac3e311706210e62ca6054875180f9
Dorus Keijzer <83274869+DorusKeijzer@users.noreply.github.com>
2024-02-22 15:23:04 +0100

    Implemented lookuptable

--- silhouetteReconstruction.py
+++ silhouetteReconstruction.py
@@ -4,27 +4,69 @@ import glob
 import numpy as np
 import assignment
 
-# allows us to iterate over the camera folders
+def readXML(path, *args):
+    """Imports the specified variables from the xml file at path"""
+    fs = cv.FileStorage(path, cv.FILE_STORAGE_READ)
+    res = []
+    for arg in args:
+        try:
+            res.append(fs.getNode(arg).mat())
+        except:
+            pass
+    fs.release()
+    return res
+
+class camera:
+    """Stores the properties of a camera
+        path: path to the camera folder
+        matrix: camera matrix
+        distcoeffs: distortion coefficients
+        rvec: rotation vector
+        tvec: translation vector
+        table: lookup table between image points and voxels"""
+    matrix, distcoeffs, rvec, tvec, table = ..., ..., ... ,... , ...
+    
+    def __init__(self, path):
+        self.path = path
+        self.matrix, self.distcoeffs, self.rvec, self.tvec = readXML(path+"configs.xml", "CameraMatrix", "DistanceCoeffs", "Rvec", "Tvec")
+        
+    def initializeTable(self, voxelgrid):
+        """Sets up the lookup table"""
+        self.table = table()
+        self.table.populateTable(voxelgrid, self)
+
 cams = glob.glob("../data/*/")
 
+cameras = []
+for cam in cams:
+    cameras.append(camera(cam))
+
 class lookuptableData:
-    def __init__(self, cam, voxel_coords, image_coords):
-        self.cam = cam
+    """Format for the data inside the lookup table"""
+    voxel_coords, image_coords = ..., ...
+
+    def __init__(self, voxel_coords, image_coords):
         self.voxel_coords = voxel_coords
         self.image_coords = image_coords
 
 class table:
+    """A lookup table mapping voxels to image pixels for a given camera"""
     def __init__(self):
         self.table = []
 
-    def addPixel(self, item):
+    def add(self, item):
         self.table.append(item)
 
-    def createTable(self, voxels):
+    def populateTable(self, voxels, cam: camera):
+        """Projects every voxel onto image pixels"""
         for voxel in voxels:
-            for i, cam in enumerate(cams): 
-                x_img, y_img = project(voxel, cam)
-                self.addPixel(lookuptableData(i, voxel, (x_img, y_img)))
+            x_img, y_img = project(voxel, cam)
+            self.add(lookuptableData(voxel, (x_img, y_img)))
+    
+def project(voxel, cam: camera):
+    """Projects a 3D voxel onto the 2D image of the specified camera"""
+    imagepoints, _ = cv.projectPoints(voxel, cam.rvec, cam.tvec, cam.matrix, cam.distcoeffs)
+    return imagepoints
 
 class MyIterator:
     def __init__(self, data):
@@ -42,9 +84,6 @@ class MyIterator:
         else:
             raise StopIteration
 
-def project():
-    raise NotImplementedError
-
 def isForeGround(pixel):
     raise NotImplementedError
 
@@ -60,8 +99,3 @@ if __name__ == "__main__":
             x = True
 
     print(image)
-
-
-
-    LookupTable = table()
-    LookupTable.createTable(voxels)
