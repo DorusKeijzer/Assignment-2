99a8e1bd5503744777f08849a4c81bfd817cfca3
Dorus Keijzer <83274869+DorusKeijzer@users.noreply.github.com>
2024-02-26 11:52:11 +0100

    Positions work, rotations don't

--- assignment.py
+++ assignment.py
@@ -1,9 +1,13 @@
 import glm
 import random
 import numpy as np
+from utils import readXML
+import glob
+import cv2 as cv
 
 block_size = 1.0
 
+camera_paths = glob.glob("data/*/")
 
 def generate_grid(width, depth):
     # Generates the floor grid locations
@@ -32,20 +36,33 @@ def set_voxel_positions(width, height, depth):
 def get_cam_positions():
     # Generates dummy camera locations at the 4 corners of the room
     # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
-    return [[-64 * block_size, 64 * block_size, 63 * block_size],
-            [63 * block_size, 64 * block_size, 63 * block_size],
-            [63 * block_size, 64 * block_size, -64 * block_size],
-            [-64 * block_size, 64 * block_size, -64 * block_size]], \
-        [[1.0, 0, 0], [0, 1.0, 0], [0, 0, 1.0], [1.0, 1.0, 0]]
+    translations = []
+
+    for camera_path in camera_paths:
+        tvec, rvec   = readXML(camera_path + "configs.xml", "Tvec", "Rvec")
+        R, _ = cv.Rodrigues(rvec)
+        translations.append(-np.dot(R.T, tvec).flatten())
+    translations = np.array([[x,-z,y] for x,y,z in translations]) # TODO not sure why -z instead of z
+    translations = translations / 100 # converts mm to m, to make it fit on the scene
+    
+    return translations, [[1.0, 0, 0], [0, 1.0, 0], [0, 0, 1.0], [1.0, 1.0, 0]]
 
 
 def get_cam_rotation_matrices():
     # Generates dummy camera rotation matrices, looking down 45 degrees towards the center of the room
     # TODO: You need to input the estimated camera rotation matrices (4x4) of the 4 cameras in the world coordinates.
-    cam_angles = [[0, 45, -45], [0, 135, -45], [0, 225, -45], [0, 315, -45]]
-    cam_rotations = [glm.mat4(1), glm.mat4(1), glm.mat4(1), glm.mat4(1)]
-    for c in range(len(cam_rotations)):
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][0] * np.pi / 180, [1, 0, 0])
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][1] * np.pi / 180, [0, 1, 0])
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1])
+    # cam_angles =[]
+    cam_rotations = []
+    for camera_path in camera_paths:
+        [rvec] = readXML(camera_path + "configs.xml", "Rvec")
+        R, _ = cv.Rodrigues(rvec) # turns rvec into rotation matrix
+        print(R)
+        rotation_matrix = glm.mat4(
+            R[0][0], R[1][0], R[2][0], 0,
+            R[0][1], R[1][1], R[2][1], 0,
+            R[0][2], R[1][2], R[2][2], 0,
+            0, 0, 0, 1
+        )
+        cam_rotations.append(rotation_matrix)
+        
     return cam_rotations
