d6e51e90abac222d443a879dac19b0b73b964576
tijn47 <159795335+tijn47@users.noreply.github.com>
2024-02-28 08:50:37 +0000

    added frame averaging and error handling
    heb geexperimenteerd met andere gaussian blurring afmetingen en verschillende manieren om de output te verbeteren maar dit lijkt toch het beste te werken. Meer frames toevoegen en averagen lijkt het resultaat alleen maar slechter te maken (hoeveel eerste frames je wilt gebruiken kan je aangeven in regel 6)

--- imgsubtr.py
+++ imgsubtr.py
@@ -3,6 +3,7 @@ import numpy as np
 import glob 
 
 cams = glob.glob("data/*/")
+num_frames = 1
 
 if __name__ == "__main__":
     for cam in cams:
@@ -10,21 +11,45 @@ if __name__ == "__main__":
         backg = cv.VideoCapture(cam + "background.avi")
         cap = cv.VideoCapture(cam + "video.avi")
         frame_rate = cap.get(cv.CAP_PROP_FPS)
+        total_gray = None
 
-        # TODO hier nog de averaging van de frames aan toevoegen
-        _, first_frame = backg.read()
-        first_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
-        first_gray = cv.GaussianBlur(first_gray, (5, 5), 0)
+        for _ in range(num_frames):
+            _, frame = backg.read()
+            if frame is not None:
+                gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                if total_gray is None:
+                    total_gray = gray_frame.copy()
+                else:
+                    total_gray += gray_frame
+            else:
+                print("Error: Unable to read the frame from the video.")
+                break
+    
+        if total_gray is not None:
+            avg_gray = total_gray / num_frames
+    
+        else:
+            print("Error: Unable to process the first frames.")
+    else:
+        print("Error: Unable to open the video file or stream.")
+
+
+        # # TODO hier nog de averaging van de frames aan toevoegen
+        # _, first_frame = backg.read()
+        # avg_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+        
+        
+        avg_gray = cv.GaussianBlur(avg_gray, (5, 5), 0)
     
         frame_width = int(cap.get(3)) 
         frame_height = int(cap.get(4)) 
         size = (frame_width, frame_height) 
-        # TODO voorbereiden bestand
         
+        # TODO voorbereiden bestand
         fourcc = cv.VideoWriter_fourcc('M','J','P','G')
 
         # writes to data/camX/subtracted.avi
-        result = cv.VideoWriter(cam + 'subtracted.avi',  
+        result = cv.VideoWriter(cam + 'subtracted.mov',  
                                 fourcc, 
                                 frame_rate, size) 
 
@@ -33,12 +58,16 @@ if __name__ == "__main__":
             if ret:
                 gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
                 gray_frame = cv.GaussianBlur(gray_frame, (5, 5), 0)
-                difference = cv.absdiff(first_gray, gray_frame)
+                gray_frame_float = np.asfarray(gray_frame,dtype = float)
+                difference = cv.absdiff(avg_gray, gray_frame_float)
                 _, difference = cv.threshold(difference, 25, 255, cv.THRESH_BINARY)
                 # cv.imshow("First frame", first_frame)
                 # cv.imshow("Frame", frame)
-                if len(difference.shape) == 2:
-                    difference = cv.cvtColor(difference, cv.COLOR_GRAY2BGR)
+                
+                
+                # if len(difference.shape) == 2:
+                #     difference = cv.cvtColor(difference, cv.COLOR_GRAY2BGR)
+
 
                 cv.imshow("difference", difference)
                 result.write(difference)
